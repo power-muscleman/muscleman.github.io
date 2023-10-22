0dbad81fde2eacd4cdd38294f4597f34d381919c
yasu <suya44574@gmail.com>
2023-10-22 15:56:00 +0900

    banner作成（仮）

--- mysite/src/App.tsx
+++ mysite/src/App.tsx
@@ -1,9 +1,11 @@
+import Banner from "./banner";
 import Header from "./header";
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
--- mysite/src/main.tsx
+++ mysite/src/main.tsx
@@ -7,6 +7,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <div className="">
+      <App />
+    </div>
   </React.StrictMode>
 );
