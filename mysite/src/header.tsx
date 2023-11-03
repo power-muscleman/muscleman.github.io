3c1abd6271730cc97a3c1b7261bd7e0af17f365d
yasu <suya44574@gmail.com>
2023-11-03 23:15:13 +0900

    バナー固定化

--- mysite/src/header.tsx
+++ mysite/src/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex items-center text-center h-[65vh] justify-center bg-gradient-to-r from-sky-500 to-indigo-500  ">
+    <header className="flex items-center text-center h-[540px] justify-center bg-gradient-to-r from-sky-500 to-indigo-500  ">
       <div className="text-white">
         <h1 className="text-7xl mb-10">Tomoya Yasuda Portfolio</h1>
         <span className="text-3xl leading-none tracking-[.08em]">
