0096736c7917b7c5a659d407d4511389a950d55b
yasu <suya44574@gmail.com>
2023-11-28 23:59:17 +0900

    fix up! base追加

--- mysite/vite.config.ts
+++ mysite/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.DEV ? "/" : "/mysite/",
+  base: "/mysite/",
 
   plugins: [react()],
 });
