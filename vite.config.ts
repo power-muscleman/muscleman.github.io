eea13f6c608b5b612f866513e5c99d7cf5171b0e
yasu <suya44574@gmail.com>
2023-11-29 18:20:37 +0900

    base変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/muscleman.github.io/",
 
   plugins: [react()],
 });
